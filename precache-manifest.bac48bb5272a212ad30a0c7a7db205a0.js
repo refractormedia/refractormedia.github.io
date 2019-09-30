2c79af1fd5dff9cb1ac44777133042f873706cf2
Kirk Min <kirk@baller.tv>
2019-09-29 21:33:38 -0700

    Updates

---
+++ precache-manifest.bac48bb5272a212ad30a0c7a7db205a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcda1290bb0078b018a95f1cbe8c91e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed29261efbefc87cdc52",
+    "url": "/static/css/main.07dba52d.chunk.css"
+  },
+  {
+    "revision": "5655869f3727e3323d36",
+    "url": "/static/js/2.a20d9cfd.chunk.js"
+  },
+  {
+    "revision": "ed29261efbefc87cdc52",
+    "url": "/static/js/main.cd3fd7a9.chunk.js"
+  },
+  {
+    "revision": "2712b051cf0017d754a6",
+    "url": "/static/js/runtime-main.d889992a.js"
+  },
+  {
+    "revision": "3b81dcff384c3b06d2eadea0d54b65fe",
+    "url": "/static/media/Favorit-Regular.3b81dcff.woff"
+  },
+  {
+    "revision": "99e0862d30ba35987b1dc8d794b78301",
+    "url": "/static/media/Spectral-Light.99e0862d.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b92b4a8155aa2d68da17bc40856539c.js"
+  "/precache-manifest.bac48bb5272a212ad30a0c7a7db205a0.js"
 );
 
 self.addEventListener('message', (event) => {
